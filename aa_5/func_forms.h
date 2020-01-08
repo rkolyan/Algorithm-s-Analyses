06dae941a1d1478eaaf85e814069051907bf3432
r_kolyan <blendervik@gmail.com>
2020-01-08 18:17:16 +0300

    TODO: PEREDELIVAY VSE

---
+++ aa_5/func_forms.h
@@ -0,0 +1,9 @@
+#ifndef FUNC_FORMS_H
+#define FUNC_FORMS_H
+
+#include "error_codes.h"
+#include "func_sort.h"
+
+typedef error_t (*function_p)(type_t *, int);
+
+#endif
---
+++ aa_5/func_linear_conveyer.h
@@ -0,0 +1,4 @@
+#ifndef FUNC_LINEAR_CONVEYER_H
+#define FUNC_LINEAR_CONVEYER_H
+
+

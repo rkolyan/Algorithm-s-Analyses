3f35fbe1b8142f53f7bb7d38195992cb4adfceca
r_kolyan <blendervik@gmail.com>
2020-01-15 21:18:30 +0300

    Delete aa_5 from master

--- aa_5/func_forms.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNC_FORMS_H
-#define FUNC_FORMS_H
-
-#include "error_codes.h"
-#include "func_sort.h"
-
-typedef error_t (*function_p)(type_t *, int);
-
-#endif
--- aa_5/func_linear_conveyer.h
+++
@@ -1,4 +0,0 @@
-#ifndef FUNC_LINEAR_CONVEYER_H
-#define FUNC_LINEAR_CONVEYER_H
-
-

5c364698105839463d4461cb5f3bd8327f338a02
r_kolyan <blendervik@gmail.com>
2020-01-20 00:41:37 +0300

    Add rk_2 base

---
+++ rk_2/io.c
@@ -0,0 +1,19 @@
+#include "io.h"
+#include <stdlib.h>
+#include <string.h>
+
+error_t create_and_input_strings(char **strings, int count)
+{
+	if (!strings || !count)
+		return ERROR_INPUT;
+	char *buffer = malloc(sizeof(char) * count);
+	for (int i = 0; i < count; i++)
+	{
+		printf("Введите %d-ю строку:");
+		fgets(buffer, BUFFER_SIZE, stdin);
+		buffer[strlen(buffer) - 1] = '\0';
+		strings[i] = strdup(buffer);
+	}
+	free(buffer);
+	return SUCCESS;
+}
---
+++ rk_2/io.h
@@ -0,0 +1,10 @@
+#ifndef IO_H
+#define IO_H
+
+#include "error_codes.h"
+
+#define BUFFER_SIZE 1000
+
+error_t create_and_input_strings(char **strings, int count);
+
+#endif

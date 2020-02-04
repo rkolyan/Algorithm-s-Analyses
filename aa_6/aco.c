ebd498f6a5f829beb496a9a8e0e0192835aacc3f
r_kolyan <blendervik@gmail.com>
2020-02-04 23:31:44 +0300

    Prepare for fix

--- aa_6/aco.c
+++ aa_6/aco.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <math.h>
  
+//Не изменять
 static error_t fill_pheromones_and_attractions_default(double **pheromone_matrix, double **attraction_matrix, double **distance_matrix, int vertex_count)
 {
 	if (!pheromone_matrix || !attraction_matrix || !distance_matrix || !vertex_count)
@@ -20,7 +21,8 @@ static error_t fill_pheromones_and_attractions_default(double **pheromone_matrix
 	return SUCCESS;
 }
 
-static error_t update_pheromones(double **pheromone_matrix, int vertex_count, parameters_t *parameters)//TODO: Add parametrisation
+//Не изменять, не трогать!
+static error_t update_pheromones(double **pheromone_matrix, int vertex_count, parameters_t *parameters)
 {
 	if (!pheromone_matrix || !vertex_count)
 		return ERROR_INPUT;
@@ -64,6 +66,7 @@ static error_t choose_way_probably(double **distance_matrix, double **pheromone_
 	return SUCCESS;
 }
 
+/*
 error_t update_pheromones_for_path(double **pheromone_matrix, list_t *head, way_t *ant, parameters_t *parameters)
 {
 	if (!pheromone_matrix || !head || !ant || !parameters)
@@ -78,75 +81,19 @@ error_t update_pheromones_for_path(double **pheromone_matrix, list_t *head, way_
 	while (current->next != head);
 	return SUCCESS;
 }
-
-error_t set_new_ant_info(way_t *ant, int start_vertice)
-{
-	if (!ant || start_vertice < 0)
-		return ERROR_INPUT;
-	delete_list(&(ant->list));
-	ant->start_vertice = start_vertice;
-	ant->length = 0;
-	return SUCCESS;
-}
-
-error_t add_finish_start(way_t *current_way, double **distance_matrix, int *result)
-{
-	if (!current_way || !distance_matrix || !result)
-		return ERROR_INPUT;
-	if (distance_matrix[current_way->list->num][current_way->list->previous->num] >= 0)
-	{
-		current_way->length += distance_matrix[current_way->list->num][current_way->list->previous->num];
-		*result = 1;
-	}
-	else
-	{
-		*result = -1;
-	}
-	return SUCCESS;
-}
+*/
 
 error_t find_commivoyager_way_aco(double **distance_matrix, double **pheromone_matrix, double **attraction_matrix, int vertices_count, way_t *ants, int ant_count, parameters_t *parameters, way_t *way)//По идее должен работать и для ориентированных графов
 {
 	if (!distance_matrix || !pheromone_matrix || !attraction_matrix || vertices_count <= 0 || !ants || ant_count <= 0)
 		return ERROR_INPUT;
-	int next = -1, previous = -1;
-	int count = 0;
 	fill_pheromones_and_attractions_default(pheromone_matrix, attraction_matrix, distance_matrix, vertices_count);
 	for (int i = 0; i < parameters->time; i++)
 	{
+		//1)Установить муравьев в случайные города
 		for (int j = 0; j < ant_count; j++)
 		{
-			count = 0;
-			set_new_ant_info(ants + j, rand() % vertices_count);
-			previous = ants[j].start_vertice;
-			do
-			{
-				choose_way_probably(distance_matrix, pheromone_matrix, attraction_matrix, vertices_count, previous, ants + j, parameters, &next);
-				if (next != -1)
-				{
-					count++;
-					ants[j].length += distance_matrix[previous][next];
-					add_element_to_list(&(ants[j].list), next);
-					previous = next;
-				}
-			}
-			while (next != -1 && count < vertices_count - 1);
-			if (next == -1)
-				continue;
-			add_finish_start(ants + j, distance_matrix, &next);
-			if (next == -1)
-			{
-				set_new_ant_info(ants + j, 0);//Удаляется список
-				continue;
-			}
-			update_pheromones_for_path(pheromone_matrix, ants[j].list, ants, parameters);//TODO:Обновлять феромон, если муравей зашел в тупик, или нет?
-			if (!(way->list) || way->length > ants[j].length)
-			{
-				delete_list(&(way->list));
-				copy_list(ants[j].list, &(way->list));
-				way->length = ants[j].length;
-			}
-			set_new_ant_info(ants + j, 0);//Удаляется список
+			//2)Сформировать маршрут для отдельного муравья
 		}
 		update_pheromones(pheromone_matrix, vertices_count, parameters);
 	}

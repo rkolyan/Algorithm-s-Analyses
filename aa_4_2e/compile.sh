bd9f586e0a94572418236dec75a325a4d59cbab3
r_kolyan <blendervik@gmail.com>
2020-02-09 17:35:40 +0300

    Temporary deleting aa_4_2e

--- aa_4_2e/compile.sh
+++
@@ -1,4 +0,0 @@
-rm -rf calculations
-mkdir calculations
-gcc -std=c11 -Wall -Werror -pedantic -ggdb -c *.c
-gcc -std=c11 -Wall -Werror -pedantic -pthread -ggdb -o user.exe io.o func_matrix_mult.o user_main.o func_matrix_creation.o

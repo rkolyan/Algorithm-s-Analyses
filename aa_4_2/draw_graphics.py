0a6c84c461be9f8b733baf555d5e5455a2fb4a79
r_kolyan <blendervik@gmail.com>
2020-02-13 00:29:05 +0300

    Tmp change exp_main

---
+++ aa_4_2/draw_graphics.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plot
+
+def fill_array_of_results(filename):
+    yarray = [];
+    tmp_file = open(filename, "r");
+    for line in tmp_file:
+        yarray.append(float(line));
+    tmp_file.close();
+    return np.array(yarray);
+
+def draw_graphic(xarray, yarrays):
+    figure = plot.figure();
+    plot.plot(xarray, yarrays[0], label = 'Стандартный алгоритм');
+    plot.plot(xarray, yarrays[1], label = 'Алгоритм Винограда');
+    plot.legend();
+    plot.title('Время умножения матриц размерностями 100x100');
+    plot.ylabel('Реальное время, секунды');
+    plot.xlabel('Количество потоков');
+    plot.grid(True);
+    plot.show();
+
+if __name__ == "__main__":
+    yarrays = list();
+    yarrays.append(fill_array_of_results("result100s.txt"));
+    yarrays.append(fill_array_of_results("result100v.txt"));
+    xarray = [];
+    for i in range(1, len(yarrays[0]) + 1):
+        xarray.append(i);
+    draw_graphic(np.array(xarray), yarrays);
--- aa_4_2/exp_main.c
+++ aa_4_2/exp_main.c
@@ -1,65 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 #include <threads.h>
-#include <time.h>
 
 #include "func_matrix_creation.h"
 #include "func_matrix_mult.h"
-#include "func_matrix_mult2.h"
 
-#define MATRIX_SIZE 2000
-#define THREADS_COUNT 8
+#define MATRIX_SIZE 1000
+#define THREADS_COUNT 32
+#define ITERATIONS_COUNT 20
 
 int main(void)
 {
-	time_t timer;
+	struct timeval timer1, timer2;
 	int **matrix1 = NULL, **matrix2 = NULL, **matrix_result = NULL;
-	int *row_factor = NULL, *col_factor = NULL;
 	thrd_t *threads = NULL;
+	double result = 0;
 	function_resourse_t *fr = NULL;
 	create_matrix(MATRIX_SIZE, MATRIX_SIZE, &matrix1);
-	fill_random_matrix_with_size(matrix1, MATRIX_SIZE, MATRIX_SIZE);
 	create_matrix(MATRIX_SIZE, MATRIX_SIZE, &matrix2);
-	fill_random_matrix_with_size(matrix2, MATRIX_SIZE, MATRIX_SIZE);
 	create_matrix(MATRIX_SIZE, MATRIX_SIZE, &matrix_result);
-	fill_random_matrix_with_size(matrix_result, MATRIX_SIZE, MATRIX_SIZE);
-	fr = malloc(sizeof(function_resourse_t) * THREADS_COUNT);
-	threads = malloc(sizeof(thrd_t) * THREADS_COUNT);
 
-	timer = time(0);
-	standard_multiply_matrix(matrix1, matrix2, MATRIX_SIZE, MATRIX_SIZE, MATRIX_SIZE, MATRIX_SIZE, threads, THREADS_COUNT, fr, matrix_result);
-	printf("%lf\n", difftime(time(0), timer));
-	fill_random_matrix_with_size(matrix1, MATRIX_SIZE, MATRIX_SIZE);
-	fill_random_matrix_with_size(matrix2, MATRIX_SIZE, MATRIX_SIZE);
-	fill_random_matrix_with_size(matrix_result, MATRIX_SIZE, MATRIX_SIZE);
+	for (int t = 1; t <= THREADS_COUNT; t++)
+	{
+		fr = malloc(sizeof(function_resourse_t) * t);
+		threads = malloc(sizeof(thrd_t) * t);
+		result = 0;
+		for (int j = 0; j < ITERATIONS_COUNT; j++)
+		{
+			fill_random_matrix_with_size(matrix1, MATRIX_SIZE, MATRIX_SIZE);
+			fill_random_matrix_with_size(matrix2, MATRIX_SIZE, MATRIX_SIZE);
+			fill_random_matrix_with_size(matrix_result, MATRIX_SIZE, MATRIX_SIZE);
 
-	create_vinograd_array(matrix1, MATRIX_SIZE, MATRIX_SIZE, 1, &row_factor);
-	create_vinograd_array(matrix2, MATRIX_SIZE, MATRIX_SIZE, 0, &col_factor);
+			gettimeofday(&timer1, NULL);
+			standard_multiply_matrix(matrix1, matrix2, MATRIX_SIZE, MATRIX_SIZE, MATRIX_SIZE, MATRIX_SIZE, threads, t, fr, matrix_result);
+			gettimeofday(&timer2, NULL);
+			result += (double)(timer2.tv_sec - timer1.tv_sec) + (timer2.tv_usec - timer1.tv_usec) / 1000000.0;
+		}
+		printf("%lf\n", result / ITERATIONS_COUNT);
+		free(threads);
+		free(fr);
+	}
+	puts("This is Vinograd");
 
-	timer = time(0);
-	vinograd_multiply_matrix(matrix1, matrix2, row_factor, col_factor, MATRIX_SIZE, MATRIX_SIZE, MATRIX_SIZE, MATRIX_SIZE, threads, THREADS_COUNT, fr, matrix_result);
-	printf("%lf\n", difftime(time(0), timer));
-	fill_random_matrix_with_size(matrix1, MATRIX_SIZE, MATRIX_SIZE);
-	fill_random_matrix_with_size(matrix2, MATRIX_SIZE, MATRIX_SIZE);
-	fill_random_matrix_with_size(matrix_result, MATRIX_SIZE, MATRIX_SIZE);
+	int *row_factor = NULL, *col_factor = NULL;
+	for (int t = 1; t <= THREADS_COUNT; t++)
+	{
+		fr = malloc(sizeof(function_resourse_t) * t);
+		threads = malloc(sizeof(thrd_t) * t);
+		result = 0;
+		for (int j = 0; j < ITERATIONS_COUNT; j++)
+		{
+			fill_random_matrix_with_size(matrix1, MATRIX_SIZE, MATRIX_SIZE);
+			fill_random_matrix_with_size(matrix2, MATRIX_SIZE, MATRIX_SIZE);
+			fill_random_matrix_with_size(matrix_result, MATRIX_SIZE, MATRIX_SIZE);
+			create_vinograd_array(matrix1, MATRIX_SIZE, MATRIX_SIZE, 1, &row_factor);
+			create_vinograd_array(matrix2, MATRIX_SIZE, MATRIX_SIZE, 0, &col_factor);
 
-	timer = time(0);
-	standard_multiply_matrix2(matrix1, matrix2, MATRIX_SIZE, MATRIX_SIZE, MATRIX_SIZE, MATRIX_SIZE, matrix_result);
-	printf("%lf\n", difftime(time(0), timer));
-	fill_random_matrix_with_size(matrix1, MATRIX_SIZE, MATRIX_SIZE);
-	fill_random_matrix_with_size(matrix2, MATRIX_SIZE, MATRIX_SIZE);
-	fill_random_matrix_with_size(matrix_result, MATRIX_SIZE, MATRIX_SIZE);
-
-	timer = time(0);
-	vinograd_multiply_matrix2(matrix1, matrix2, row_factor, col_factor, MATRIX_SIZE, MATRIX_SIZE, MATRIX_SIZE, MATRIX_SIZE, matrix_result);
-	printf("%lf\n", difftime(time(0), timer));
+			gettimeofday(&timer1, NULL);
+			vinograd_multiply_matrix(matrix1, matrix2, row_factor, col_factor, MATRIX_SIZE, MATRIX_SIZE, MATRIX_SIZE, MATRIX_SIZE, threads, t, fr, matrix_result);
+			gettimeofday(&timer2, NULL);
+			result += (double)(timer2.tv_sec - timer1.tv_sec) + (timer2.tv_usec - timer1.tv_usec) / 1000000.0;
+			free(col_factor);
+			free(row_factor);
+		}
+		printf("%lf\n", result / ITERATIONS_COUNT);
+		free(threads);
+		free(fr);
+	}
 
 	free(matrix1);
 	free(matrix2);
 	free(matrix_result);
-	free(row_factor);
-	free(col_factor);
-	free(threads);
-	free(fr);
 	return 0;
 }

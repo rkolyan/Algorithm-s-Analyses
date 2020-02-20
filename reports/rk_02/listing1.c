7b379927ca0596b00b0168681044c89201b196c8
r_kolyan <blendervik@gmail.com>
2020-02-20 17:25:48 +0300

    Update listing

--- reports/rk_02/listing1.c
+++ reports/rk_02/listing1.c
@@ -1,35 +1,49 @@
 #include "dictionary.h"
 #include <stdlib.h>
 
-error_t is_word_in_dictionary(dictionary_t **dictionary, char *word, int length, char *result)
+error_t find_position_and_length_of_maximum_substring(dictionary_t **dictionary, char *string, int length, int *sposition, int *slength)
 {
-
-	if (!dictionary || !word || !result)
+	if (!dictionary || !string || !sposition || !length || !slength)
 		return ERROR_INPUT;
-	if (!length)
-		return SUCCESS;
-	
-	int delta_symbol = return_symbol_delta_code(*word);
-	int position = 0;
-	*result = 0;
 
-	if (delta_symbol < 0)
-		return SUCCESS;
+	int max_position = -1, max_length = 0;
+	int current_length = 0, current_position = -1;
+	char flag = 0;
+	dictionary_t *current_node = NULL;
 
-	dictionary_t *current_node = dictionary[delta_symbol];
-	if (!current_node)
-		return SUCCESS;
-
-	for (int i = 1; i < length; i++)
+	for (int i = 0; i < length - 1; i++)
 	{
-		delta_symbol = return_symbol_delta_code(word[i]);
-		if (delta_symbol == -1 || !(position = current_node->next_symbol_positions[delta_symbol]))
-			return SUCCESS;
-		current_node = current_node->nexts[position - 1];
+		if (!flag)
+		{
+			current_node = dictionary[(int)return_symbol_delta_code(string[i])];
+			if (current_node)
+			{
+				current_length = 1;
+				current_position = i;
+				if (current_node->next_symbol_positions[(int)return_symbol_delta_code(string[i + 1])])
+					flag = 1;
+			}
+			else
+			{
+				current_length = 0;
+			}
+		}
+		else
+		{
+			current_node = current_node->nexts[current_node->next_symbol_positions[(int)return_symbol_delta_code(string[i])] - 1];
+			current_length++;
+			if (!current_node->next_symbol_positions[(int)return_symbol_delta_code(string[i + 1])])
+				flag = 0;
+		}
+		if (current_length > max_length)
+		{
+			max_length = current_length;
+			max_position = current_position;
+		}
 	}
-
-	if (current_node->is_next_null)
-		*result = 1;
-
+	if (flag)
+		current_length++;
+	*sposition = max_position;
+	*slength = max_length;
 	return SUCCESS;
 }
